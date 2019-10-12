14e328171a8138d2d8efe464ee231509347037d2
Herb Caudill <herb@devresults.com>
2019-10-12 14:08:10 +0200

    Calculate.tsx

--- demo/Calculate.tsx
+++ demo/Calculate.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react'
+﻿import * as React from 'react'
 import { actions } from './redux/actions'
 import { State } from './redux/reducer'
 import { Dispatch } from 'redux'
@@ -6,7 +6,13 @@ import { Dispatch } from 'redux'
 // When doing calculations in UI thread, React won't update at all if we don't do this
 export const nextFrame = () => new Promise(ok => requestAnimationFrame(ok))
 
-export const Calculate = ({ state, dispatch }: { state: State; dispatch: Dispatch }) => {
+export const Calculate = ({
+  state,
+  dispatch,
+}: {
+  state: State
+  dispatch: Dispatch
+}) => {
   const calculate = async () => {
     dispatch(actions.setBusy(true))
     await nextFrame()
