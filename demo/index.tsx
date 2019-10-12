e1befd3ad1835e6ef9b4b45b32d8c09a1d1e8385
Herb Caudill <herb@devresults.com>
2019-10-12 16:39:21 +0200

    demo/index.tsx: not async any more

--- demo/index.tsx
+++ demo/index.tsx
@@ -9,38 +9,34 @@ import { Clock } from './Clock'
 import { WithoutWorker } from './WithoutWorker'
 import { WithWorker } from './WithWorker'
 
-const start = async () => {
-  // Set up proxy provider
-  const worker = new Worker('./redux/worker.ts')
-  const ProxyProvider = getProvider(worker)
+// Set up proxy provider
+const worker = new Worker('./redux/worker.ts')
+const ProxyProvider = getProvider(worker)
 
-  // Set up regular provider
-  const RegularProvider = ({ children }) => (
-    <Provider store={store}>{children}</Provider>
-  )
+// Set up regular provider
+const RegularProvider = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+)
 
-  ReactDOM.render(
-    <div>
-      <Clock></Clock>
-      <p>Use the buttons to find a few large prime numbers.</p>
-      <p>
-        Without a worker, notice how the animation stops updating every time the
-        app calculates a new prime.
-      </p>
-      <div style={{ display: 'flex' }}>
-        {/* using redux-workerized */}
-        <ProxyProvider>
-          <WithWorker />
-        </ProxyProvider>
+ReactDOM.render(
+  <div>
+    <Clock></Clock>
+    <p>Use the buttons to find a few large prime numbers.</p>
+    <p>
+      Without a worker, notice how the animation stops updating every time the
+      app calculates a new prime.
+    </p>
+    <div style={{ display: 'flex' }}>
+      {/* using redux-workerized */}
+      <ProxyProvider>
+        <WithWorker />
+      </ProxyProvider>
 
-        {/* using regular redux */}
-        <RegularProvider>
-          <WithoutWorker />
-        </RegularProvider>
-      </div>
-    </div>,
-    document.querySelector('.root')
-  )
-}
-
-start()
+      {/* using regular redux */}
+      <RegularProvider>
+        <WithoutWorker />
+      </RegularProvider>
+    </div>
+  </div>,
+  document.querySelector('.root')
+)
