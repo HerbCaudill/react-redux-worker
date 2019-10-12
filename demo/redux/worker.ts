a3e9b2a23e1e83381b4bb792fc002b42c52d0e83
Herb Caudill <herb@devresults.com>
2019-10-12 13:08:23 +0200

    worker.ts

--- demo/redux/worker.ts
+++ demo/redux/worker.ts
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import reducer from './reducer'
-import { expose, createProxyStore } from '../../src'
+import { expose, createProxyStore } from 'react-redux-worker'
 
 const store = createStore(reducer)
 const proxyStore = createProxyStore(store)
