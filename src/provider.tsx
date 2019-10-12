0c46d605c94ae3291e061a428f614be699e424d5
Herb Caudill <herb@devresults.com>
2019-10-12 14:17:16 +0200

    provider.tsx

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import { useStore } from './hooks'
 import { StateContext } from './StateContext'
 import { StoreContext } from './StoreContext'
