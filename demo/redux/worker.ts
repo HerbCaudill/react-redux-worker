97defcd656b233a6d2caaa444324f2bde58ff704
Herb Caudill <herb@devresults.com>
2019-10-12 16:31:29 +0200

    store to own file

--- demo/redux/worker.ts
+++ demo/redux/worker.ts
@@ -1,8 +1,5 @@
-import { createStore } from 'redux'
-import reducer from './reducer'
 import { expose, createProxyStore } from '../../src' // replace with react-redux-worker
+import { store } from './store'
 
-const store = createStore(reducer)
 const proxyStore = createProxyStore(store)
-
 expose(proxyStore, self)
