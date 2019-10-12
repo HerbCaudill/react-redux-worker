0d1863f8d90205c97886452f09db19bc37c8de46
Herb Caudill <herb@devresults.com>
2019-10-12 14:04:37 +0200

    proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,4 @@
 import * as Comlink from 'comlinkjs'
-import { AnyAction } from 'redux'
-
-export type ProxyStore<State, A extends AnyAction = AnyAction> = {
-  getState(): Promise<State>
-  dispatch(action: A): Promise<void>
-  subscribe(
-    listener: (state: State) => void,
-    selector?: (root: State) => Promise<State> | State
-  ): Promise<number>
-  unsubscribe(listenerId: number): Promise<void>
-}
 
 let lastId = 0
 export const uniqueId = () => ++lastId
